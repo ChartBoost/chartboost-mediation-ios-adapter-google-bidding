ca7c4d5ff53074c6a98ff38eed724c404f7f767b
Alex Rice <rice.alexanderb@gmail.com>
2022-11-08 12:16:27 -0800

    HB-4469: implement banner, interstitial, and rewarded ads

---
+++ Source/GoogleBiddingAdapterBannerAd.swift
@@ -0,0 +1,97 @@
+//
+// GoogleBiddingAdapterInterstitialAd.swift
+// GoogleBiddingAdapter
+//
+// Created by Alex Rice on 10/03/22
+//
+
+import Foundation
+import GoogleMobileAds
+import HeliumSdk
+
+class GoogleBiddingAdapterBannerAd: GoogleBiddingAdapterAd, PartnerAd {
+    
+    /// The partner ad view to display inline. E.g. a banner view.
+    /// Should be nil for full-screen ads.
+    var inlineView: UIView?
+    
+    // The GoogleBidding Ad Object
+    var ad: GADBannerView?
+    
+    /// Loads an ad.
+    /// - parameter viewController: The view controller on which the ad will be presented on. Needed on load for some banners.
+    /// - parameter completion: Closure to be performed once the ad has been loaded.
+    func load(with viewController: UIViewController?, completion: @escaping (Result<PartnerEventDetails, Error>) -> Void) {
+        loadCompletion = completion
+
+        // Banner ads auto-show after loading, so we must have a ViewController
+        guard viewController != nil else {
+            let error = error(.noViewController)
+            log(.loadFailed(error))
+            completion(.failure(error))
+            return
+        }
+        
+        let gbRequest = generateRequest()
+        gbRequest.adString = request.adm
+
+        let placementID = request.partnerPlacement
+
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else { return }
+            let size = self.gadAdSizeFrom(cgSize: self.request.size)
+            self.ad = GADBannerView(adSize: size)
+            self.ad?.adUnitID = placementID
+            self.ad?.isAutoloadEnabled = false
+            self.ad?.delegate = self
+            self.ad?.rootViewController = viewController
+            self.ad?.load(gbRequest)
+        }
+    }
+    
+    /// Shows a loaded ad.
+    /// It will never get called for banner ads. You may leave the implementation blank for that ad format.
+    /// - parameter viewController: The view controller on which the ad will be presented on.
+    /// - parameter completion: Closure to be performed once the ad has been shown.
+    func show(with viewController: UIViewController, completion: @escaping (Result<PartnerEventDetails, Error>) -> Void) {
+        // no-op
+    }
+    
+    func gadAdSizeFrom(cgSize: CGSize?) -> GADAdSize {
+        guard let size = cgSize else { return GADAdSizeInvalid }
+        switch (size.width, size.height) {
+        case (320, 50):
+            return GADAdSizeBanner
+        case (300, 250):
+            return GADAdSizeMediumRectangle
+        case (728, 90):
+            return GADAdSizeLeaderboard
+        default:
+            return GADAdSizeInvalid
+        }
+    }
+}
+
+extension GoogleBiddingAdapterBannerAd: GADBannerViewDelegate {
+
+    func bannerViewDidReceiveAd(_ bannerView: GADBannerView) {
+        log(.loadSucceeded)
+        self.inlineView = bannerView
+        loadCompletion?(.success([:])) ?? log(.loadResultIgnored)
+    }
+
+    func bannerView(_ bannerView: GADBannerView, didFailToReceiveAdWithError error: Error) {
+        // Report load failure
+        let error = self.error(.loadFailure)
+        loadCompletion?(.failure(error)) ?? log(.loadResultIgnored)
+        loadCompletion = nil
+    }
+
+    func bannerViewDidRecordImpression(_ bannerView: GADBannerView) {
+        self.delegate?.didTrackImpression(self, details: [:]) ?? log(.delegateUnavailable)
+    }
+
+    func bannerViewDidRecordClick(_ bannerView: GADBannerView) {
+        self.delegate?.didClick(self, details: [:]) ?? log(.delegateUnavailable)
+    }
+}
---
+++ Source/GoogleBiddingAdapterRewardedAd.swift
@@ -0,0 +1,98 @@
+//
+// GoogleBiddingAdapterRewardedAd.swift
+// GoogleBiddingAdapter
+//
+// Created by Alex Rice on 10/03/22
+//
+
+import Foundation
+import GoogleMobileAds
+import HeliumSdk
+
+final class GoogleBiddingAdapterRewardedAd: GoogleBiddingAdapterAd, PartnerAd {
+    
+    /// The partner ad view to display inline. E.g. a banner view.
+    /// Should be nil for full-screen ads.
+    var inlineView: UIView? { nil }
+    
+    
+    // The GoogleBidding Ad Object
+    var ad: GADRewardedAd?
+    
+    /// Loads an ad.
+    /// - parameter viewController: The view controller on which the ad will be presented. Needed on load for some banners.
+    /// - parameter completion: Closure to be performed once the ad has been loaded.
+    func load(with viewController: UIViewController?, completion: @escaping (Result<PartnerEventDetails, Error>) -> Void) {
+        log(.loadStarted)
+        
+        let gbRequest = generateRequest()
+        gbRequest.adString = request.adm
+        GADRewardedAd.load(withAdUnitID:self.request.partnerPlacement,
+                                request: gbRequest) { [weak self] ad, error in
+            guard let self = self else { return }
+            if let error = error {
+                self.log(.loadFailed(error))
+                completion(.failure(error))
+                return
+            }
+            self.ad = ad
+            ad?.fullScreenContentDelegate = self
+            completion(.success([:]))
+        }
+    }
+    
+    /// Shows a loaded ad.
+    /// It will never get called for banner ads. You may leave the implementation blank for that ad format.
+    /// - parameter viewController: The view controller on which the ad will be presented.
+    /// - parameter completion: Closure to be performed once the ad has been shown.
+    func show(with viewController: UIViewController, completion: @escaping (Result<PartnerEventDetails, Error>) -> Void) {
+        log(.showStarted)
+        
+        guard let ad = ad else {
+            let error = error(.noAdReadyToShow)
+            log(.showFailed(error))
+            completion(.failure(error))
+            return
+        }
+        showCompletion = completion
+        
+        DispatchQueue.main.async {
+            ad.present(fromRootViewController: viewController) { [weak self] in
+                guard let self = self else { return }
+                self.log(.didReward)
+                self.delegate?.didReward(self, details: [:]) ?? self.log(.delegateUnavailable)
+            }
+        }
+    }
+}
+
+extension GoogleBiddingAdapterRewardedAd: GADFullScreenContentDelegate {
+    
+    func adDidRecordImpression(_ ad: GADFullScreenPresentingAd) {
+        log(.didTrackImpression)
+        delegate?.didTrackImpression(self, details: [:]) ?? log(.delegateUnavailable)
+    }
+    
+    func adDidRecordClick(_ ad: GADFullScreenPresentingAd) {
+        log(.didClick(error: nil))
+        delegate?.didClick(self, details: [:]) ?? log(.delegateUnavailable)
+    }
+    
+    func ad(_ ad: GADFullScreenPresentingAd, didFailToPresentFullScreenContentWithError error: Error) {
+        let err = self.error(.showFailure, error: error)
+        log(.showFailed(err))
+        showCompletion?(.failure(error)) ?? log(.showResultIgnored)
+        showCompletion = nil
+    }
+    
+    // Google has deprecated adDidPresentFullScreenContent and says to use this delegate method instead
+    func adWillPresentFullScreenContent(_ ad: GADFullScreenPresentingAd) {
+        showCompletion?(.success([:])) ?? log(.showResultIgnored)
+        showCompletion = nil
+    }
+    
+    func adDidDismissFullScreenContent(_ ad: GADFullScreenPresentingAd) {
+        log(.didDismiss(error: nil))
+        delegate?.didDismiss(self, details: [:], error: nil) ?? log(.delegateUnavailable)
+    }
+}
