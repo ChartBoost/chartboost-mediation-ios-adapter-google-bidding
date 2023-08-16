37c180b0138c1ec44d826683c453802896115544
Ray Graham <rayg@bitwisedesign.com>
2023-08-16 12:37:15 -0700

    HB-5933: use os_log instead of print (#44)
    * HB-5933: use os_log instead of print
    
    * Apply suggestions from code review

--- Source/GoogleBiddingAdapterConfiguration.swift
+++ Source/GoogleBiddingAdapterConfiguration.swift
@@ -5,10 +5,13 @@
 
 import Foundation
 import GoogleMobileAds
+import os.log
 
 /// A list of externally configurable properties pertaining to the partner SDK that can be retrieved and set by publishers.
 @objc public class GoogleBiddingAdapterConfiguration: NSObject {
-    
+
+    private static let log = OSLog(subsystem: "com.chartboost.mediation.adapter.google_googlebidding", category: "Configuration")
+
     /// Google's identifier for your test device can be found in the console output from their SDK
     class func setTestDeviceId(_ id: String?) {
         if let id = id {
@@ -16,6 +19,8 @@ import GoogleMobileAds
         } else {
             GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers = []
         }
-        print("Google Bidding SDK test device ID set to \(id ?? "nil")")
+        if #available(iOS 12.0, *) {
+            os_log(.debug, log: log, "Google Bidding SDK test device ID set to %{public}s", id ?? "nil")
+        }
     }
 }
