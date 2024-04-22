82d459eb1cb99e88b169f600fe3073024a4af03c
Daniel Barros <daniel.barros@chartboost.com>
2024-04-22 10:35:45 -0700

    Remove unnecessary return keywords

--- Source/GoogleBiddingAdapter.swift
+++ Source/GoogleBiddingAdapter.swift
@@ -162,7 +162,7 @@ final class GoogleBiddingAdapter: PartnerAdapter {
     /// - parameter delegate: The delegate that will receive ad life-cycle notifications.
     func makeBannerAd(request: PartnerAdLoadRequest, delegate: PartnerAdDelegate) throws -> PartnerBannerAd {
         // This partner supports multiple loads for the same partner placement.
-        return GoogleBiddingAdapterBannerAd(adapter: self, request: request, delegate: delegate, extras: sharedExtras)
+        GoogleBiddingAdapterBannerAd(adapter: self, request: request, delegate: delegate, extras: sharedExtras)
     }
 
     /// Creates a new ad object in charge of communicating with a single partner SDK ad instance.
