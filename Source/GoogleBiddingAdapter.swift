5f7039d4afad7954c2df878ee0b5f5500c3a9f15
Helium Service <helium-service@chartboost.com>
2024-08-01 15:04:18 -0700

    Release 5.11.5.0.0

--- Source/GoogleBiddingAdapter.swift
+++ Source/GoogleBiddingAdapter.swift
@@ -19,24 +19,10 @@ enum GoogleStrings {
 }
 
 final class GoogleBiddingAdapter: PartnerAdapter {
-    
-    /// The version of the partner SDK.
-    var partnerSDKVersion: String {
-        let versionNumber = GADMobileAds.sharedInstance().versionNumber
-        return "\(versionNumber.majorVersion).\(versionNumber.minorVersion).\(versionNumber.patchVersion)"
-    }
-    
-    /// The version of the adapter.
-    /// It should have either 5 or 6 digits separated by periods, where the first digit is Chartboost Mediation SDK's major version, the last digit is the adapter's build version, and intermediate digits are the partner SDK's version.
-    /// Format: `<Chartboost Mediation major version>.<Partner major version>.<Partner minor version>.<Partner patch version>.<Partner build version>.<Adapter build version>` where `.<Partner build version>` is optional.
-    let adapterVersion = "4.11.7.0.0"
-    
-    /// The partner's unique identifier.
-    let partnerIdentifier = "google_googlebidding"
-    
-    /// The human-friendly partner name.
-    let partnerDisplayName = "Google bidding"
-    
+    /// The adapter configuration type that contains adapter and partner info.
+    /// It may also be used to expose custom partner SDK options to the publisher.
+    var configuration: PartnerAdapterConfiguration.Type { GoogleBiddingAdapterConfiguration.self }
+
     /// Parameters that should be included in all ad requests
     let sharedExtras = GADExtras()
 
@@ -47,18 +33,23 @@ final class GoogleBiddingAdapter: PartnerAdapter {
     init(storage: PartnerAdapterStorage) {
         // no-op
     }
-    
+
     /// Does any setup needed before beginning to load ads.
     /// - parameter configuration: Configuration data for the adapter to set up.
-    /// - parameter completion: Closure to be performed by the adapter when it's done setting up. It should include an error indicating the cause for failure or `nil` if the operation finished successfully.
-    func setUp(with configuration: PartnerConfiguration, completion: @escaping (Error?) -> Void) {
+    /// - parameter completion: Closure to be performed by the adapter when it's done setting up. It should include an error indicating
+    /// the cause for failure or `nil` if the operation finished successfully.
+    func setUp(with configuration: PartnerConfiguration, completion: @escaping (Result<PartnerDetails, Error>) -> Void) {
         log(.setUpStarted)
-        
+
         // Parameters that need to be on every Google bidding request
         sharedExtras.additionalParameters = [GoogleStrings.queryTypeKey: GoogleStrings.queryType]
-        
+
         // Disable Google mediation since Chartboost Mediation is the mediator
         GADMobileAds.sharedInstance().disableMediationInitialization()
+
+        // Apply initial consents
+        setConsents(configuration.consents, modifiedKeys: Set(configuration.consents.keys))
+        setIsUserUnderage(configuration.isUserUnderage)
 
         // Exit early if GoogleMobileAds SDK has already been initalized
         let statuses = GADMobileAds.sharedInstance().initializationStatus
@@ -67,116 +58,147 @@ final class GoogleBiddingAdapter: PartnerAdapter {
             log("Redundant call to initalize GoogleMobileAds was ignored")
             // We should log either success or failure before returning, and this is more like success.
             log(.setUpSucceded)
-            completion(nil)
+            completion(.success([:]))
             return
         }
-        
+
         GADMobileAds.sharedInstance().start { initStatus in
             let statuses = initStatus.adapterStatusesByClassName
             if statuses[GoogleStrings.gadClassName]?.state == .ready {
                 self.log(.setUpSucceded)
-                completion(nil)
+                completion(.success([:]))
             } else {
-                let error = self.error(.initializationFailureUnknown,
-                                       description: "GoogleBidding adapter status was \(String(describing: statuses[GoogleStrings.gadClassName]?.state))")
+                let error = self.error(
+                    .initializationFailureUnknown,
+                    description: "GoogleBidding adapter status was \(String(describing: statuses[GoogleStrings.gadClassName]?.state))"
+                )
                 self.log(.setUpFailed(error))
-                completion(error)
+                completion(.failure(error))
             }
         }
     }
-    
+
     /// Fetches bidding tokens needed for the partner to participate in an auction.
     /// - parameter request: Information about the ad load request.
     /// - parameter completion: Closure to be performed with the fetched info.
-    func fetchBidderInformation(request: PreBidRequest, completion: @escaping ([String: String]?) -> Void) {
+    func fetchBidderInformation(request: PartnerAdPreBidRequest, completion: @escaping (Result<[String: String], Error>) -> Void) {
         log(.fetchBidderInfoStarted(request))
-        
+
         let gbRequest = GADRequest()
         gbRequest.requestAgent = "Chartboost"
         gbRequest.register(sharedExtras)
-        
+
         // Convert from our internal AdFormat type to Google's ad format type
         guard let gbAdFormat = googleAdFormat(from: request.format) else {
-            let error = error(.prebidFailureUnknown, description: "Failed to map ad format \(request.format) to GADAdFormat")
+            let error = error(.prebidFailureUnsupportedAdFormat, description: "Failed to map ad format \(request.format) to GADAdFormat")
             log(.fetchBidderInfoFailed(request, error: error))
-            completion(nil)
+            completion(.failure(error))
             return
         }
 
         GADQueryInfo.createQueryInfo(with: gbRequest, adFormat: gbAdFormat) { queryInfo, error in
-            if let token = queryInfo?.query {
-                self.log(.fetchBidderInfoSucceeded(request))
-                completion(["token": token])
+            if let error {
+                self.log(.fetchBidderInfoFailed(request, error: error))
+                completion(.failure(error))
             } else {
-                let partnerError = self.error(.prebidFailureUnknown, description: "Token was nil", error: error)
-                self.log(.fetchBidderInfoFailed(request, error: partnerError))
-                completion(nil)
+                self.log(.fetchBidderInfoSucceeded(request))
+                let token = queryInfo?.query
+                completion(.success(token.map { ["token": $0] } ?? [:]))
             }
         }
     }
-    
-    /// Indicates if GDPR applies or not and the user's GDPR consent status.
-    /// - parameter applies: `true` if GDPR applies, `false` if not, `nil` if the publisher has not provided this information.
-    /// - parameter status: One of the `GDPRConsentStatus` values depending on the user's preference.
-    func setGDPR(applies: Bool?, status: GDPRConsentStatus) {
-        // The "npa" parameter is an internal AdMob feature not publicly documented, and is subject to change.
-        if applies == true && status != .granted {
-            // Set "npa" to "1" by merging with the existing extras dictionary if it's non-nil and overwriting the old value if keys collide
-            sharedExtras.additionalParameters = (sharedExtras.additionalParameters ?? [:]).merging([GoogleStrings.nonPersonalizedAdsKey:"1"], uniquingKeysWith: { (_, new) in new })
-            log(.privacyUpdated(setting: GoogleStrings.nonPersonalizedAdsKey, value: "1"))
-        } else {
-            // If GDPR doesn't apply or status is '.granted', then remove the "non-personalized ads" flag
-            sharedExtras.additionalParameters?[GoogleStrings.nonPersonalizedAdsKey] = nil
-            log(.privacyUpdated(setting: GoogleStrings.nonPersonalizedAdsKey, value: nil))
+
+    /// Indicates that the user consent has changed.
+    /// - parameter consents: The new consents value, including both modified and unmodified consents.
+    /// - parameter modifiedKeys: A set containing all the keys that changed.
+    func setConsents(_ consents: [ConsentKey: ConsentValue], modifiedKeys: Set<ConsentKey>) {
+        if modifiedKeys.contains(configuration.partnerID) || modifiedKeys.contains(ConsentKeys.gdprConsentGiven) {
+            updateGPDR()
+        }
+        if modifiedKeys.contains(ConsentKeys.ccpaOptIn) {
+            updateCCPA()
+        }
+
+        func updateGPDR() {
+            // Use a partner-specific consent if available, falling back to the general GDPR consent if not
+            if (consents[configuration.partnerID] ?? consents[ConsentKeys.gdprConsentGiven]) == ConsentValues.denied {
+                // The "npa" parameter is an internal AdMob feature not publicly documented, and is subject to change.
+                // Set "npa" to "1" by merging with the existing extras dictionary if it's non-nil and overwriting the old value if keys 
+                // collide
+                sharedExtras.additionalParameters = (sharedExtras.additionalParameters ?? [:])
+                    .merging([GoogleStrings.nonPersonalizedAdsKey: "1"], uniquingKeysWith: { _, new in new })
+                log(.privacyUpdated(setting: GoogleStrings.nonPersonalizedAdsKey, value: "1"))
+            } else {
+                // If GDPR status is granted or the info is not provided then remove the "non-personalized ads" flag
+                sharedExtras.additionalParameters?[GoogleStrings.nonPersonalizedAdsKey] = nil
+                log(.privacyUpdated(setting: GoogleStrings.nonPersonalizedAdsKey, value: nil))
+            }
+        }
+
+        func updateCCPA() {
+            // See https://developers.google.com/admob/ios/privacy/ccpa
+            let needsRestrictedDataProcessing: Bool?
+            switch consents[ConsentKeys.ccpaOptIn] {
+            case ConsentValues.granted:
+                needsRestrictedDataProcessing = false
+            case ConsentValues.denied:
+                needsRestrictedDataProcessing = true
+            default:
+                needsRestrictedDataProcessing = nil
+            }
+            if let needsRestrictedDataProcessing {
+                UserDefaults.standard.set(needsRestrictedDataProcessing, forKey: GoogleStrings.ccpaKey)
+            } else {
+                UserDefaults.standard.removeObject(forKey: GoogleStrings.ccpaKey)
+            }
+            log(.privacyUpdated(setting: GoogleStrings.ccpaKey, value: needsRestrictedDataProcessing))
         }
     }
-    
-    /// Indicates the CCPA status both as a boolean and as an IAB US privacy string.
-    /// - parameter hasGivenConsent: A boolean indicating if the user has given consent.
-    /// - parameter privacyString: An IAB-compliant string indicating the CCPA status.
-    func setCCPA(hasGivenConsent: Bool, privacyString: String) {
-        // See https://developers.google.com/admob/ios/privacy/ccpa
-        // Invert the boolean, because "has given consent" is the opposite of "needs Restricted Data Processing"
-        log(.privacyUpdated(setting: GoogleStrings.ccpaKey, value: !hasGivenConsent))
-        UserDefaults.standard.set(!hasGivenConsent, forKey: GoogleStrings.ccpaKey)
-    }
-    
-    /// Indicates if the user is subject to COPPA or not.
-    /// - parameter isChildDirected: `true` if the user is subject to COPPA, `false` otherwise.
-    func setCOPPA(isChildDirected: Bool) {
+
+    /// Indicates that the user is underage signal has changed.
+    /// - parameter isUserUnderage: `true` if the user is underage as determined by the publisher, `false` otherwise.
+    func setIsUserUnderage(_ isUserUnderage: Bool) {
         // See https://developers.google.com/admob/ios/api/reference/Classes/GADRequestConfiguration#-tagforchilddirectedtreatment:
-        log(.privacyUpdated(setting: "ChildDirectedTreatment", value: isChildDirected))
-        GADMobileAds.sharedInstance().requestConfiguration.tagForChildDirectedTreatment = NSNumber(booleanLiteral: isChildDirected)
+        log(.privacyUpdated(setting: "ChildDirectedTreatment", value: isUserUnderage))
+        GADMobileAds.sharedInstance().requestConfiguration.tagForChildDirectedTreatment = NSNumber(booleanLiteral: isUserUnderage)
     }
-    
+
+    /// Creates a new banner ad object in charge of communicating with a single partner SDK ad instance.
+    /// Chartboost Mediation SDK calls this method to create a new ad for each new load request. Ad instances are never reused.
+    /// Chartboost Mediation SDK takes care of storing and disposing of ad instances so you don't need to.
+    /// ``PartnerAd/invalidate()`` is called on ads before disposing of them in case partners need to perform any custom logic before the
+    /// object gets destroyed.
+    /// If, for some reason, a new ad cannot be provided, an error should be thrown.
+    /// Chartboost Mediation SDK will always call this method from the main thread.
+    /// - parameter request: Information about the ad load request.
+    /// - parameter delegate: The delegate that will receive ad life-cycle notifications.
+    func makeBannerAd(request: PartnerAdLoadRequest, delegate: PartnerAdDelegate) throws -> PartnerBannerAd {
+        // This partner supports multiple loads for the same partner placement.
+        GoogleBiddingAdapterBannerAd(adapter: self, request: request, delegate: delegate, extras: sharedExtras)
+    }
+
     /// Creates a new ad object in charge of communicating with a single partner SDK ad instance.
     /// Chartboost Mediation SDK calls this method to create a new ad for each new load request. Ad instances are never reused.
     /// Chartboost Mediation SDK takes care of storing and disposing of ad instances so you don't need to.
-    /// `invalidate()` is called on ads before disposing of them in case partners need to perform any custom logic before the object gets destroyed.
+    /// ``PartnerAd/invalidate()`` is called on ads before disposing of them in case partners need to perform any custom logic before the
+    /// object gets destroyed.
     /// If, for some reason, a new ad cannot be provided, an error should be thrown.
     /// - parameter request: Information about the ad load request.
     /// - parameter delegate: The delegate that will receive ad life-cycle notifications.
-    func makeAd(request: PartnerAdLoadRequest, delegate: PartnerAdDelegate) throws -> PartnerAd {
+    func makeFullscreenAd(request: PartnerAdLoadRequest, delegate: PartnerAdDelegate) throws -> PartnerFullscreenAd {
         // This partner supports multiple loads for the same partner placement.
         switch request.format {
-        case .banner:
-            return GoogleBiddingAdapterBannerAd(adapter: self, request: request, delegate: delegate, extras: sharedExtras)
-        case .interstitial:
+        case PartnerAdFormats.interstitial:
             return GoogleBiddingAdapterInterstitialAd(adapter: self, request: request, delegate: delegate, extras: sharedExtras)
-        case .rewarded:
+        case PartnerAdFormats.rewarded:
             return GoogleBiddingAdapterRewardedAd(adapter: self, request: request, delegate: delegate, extras: sharedExtras)
+        case PartnerAdFormats.rewardedInterstitial:
+            return GoogleBiddingAdapterRewardedInterstitialAd(adapter: self, request: request, delegate: delegate, extras: sharedExtras)
         default:
-            // Not using the `.rewardedInterstitial` or `.adaptive_banner` cases directly to maintain backward compatibility with Chartboost Mediation 4.0
-            if request.format.rawValue == "rewarded_interstitial" {
-                return GoogleBiddingAdapterRewardedInterstitialAd(adapter: self, request: request, delegate: delegate, extras: sharedExtras)
-            } else if request.format.rawValue == "adaptive_banner" {
-                return GoogleBiddingAdapterBannerAd(adapter: self, request: request, delegate: delegate, extras: sharedExtras)
-            } else {
-                throw error(.loadFailureUnsupportedAdFormat)
-            }
+            throw error(.loadFailureUnsupportedAdFormat)
         }
     }
-    
+
     /// Maps a partner load error to a Chartboost Mediation error code.
     /// Chartboost Mediation SDK calls this method when a load completion is called with a partner error.
     ///
@@ -223,7 +245,7 @@ final class GoogleBiddingAdapter: PartnerAdapter {
             return nil
         }
     }
-    
+
     /// Maps a partner show error to a Chartboost Mediation error code.
     /// Chartboost Mediation SDK calls this method when a show completion is called with a partner error.
     ///
@@ -252,24 +274,19 @@ final class GoogleBiddingAdapter: PartnerAdapter {
             return nil
         }
     }
-    
-    func googleAdFormat(from adFormat: AdFormat) -> GADAdFormat? {
+
+    func googleAdFormat(from adFormat: PartnerAdFormat) -> GADAdFormat? {
         switch adFormat {
-        case .banner:
+        case PartnerAdFormats.banner:
             return GADAdFormat.banner
-        case .interstitial:
+        case PartnerAdFormats.interstitial:
             return GADAdFormat.interstitial
-        case .rewarded:
+        case PartnerAdFormats.rewarded:
             return GADAdFormat.rewarded
+        case PartnerAdFormats.rewardedInterstitial:
+            return GADAdFormat.rewardedInterstitial
         default:
-            // Not using the `.rewardedInterstitial` or `.adaptive_banner` cases directly to maintain backward compatibility with Chartboost Mediation 4.0
-            if adFormat.rawValue == "rewarded_interstitial" {
-                return GADAdFormat.rewardedInterstitial
-            } else if adFormat.rawValue == "adaptive_banner" {
-                return GADAdFormat.banner
-            } else {
-                return nil
-            }
+            return nil
         }
     }
 }
--- Source/GoogleBiddingAdapterBannerAd.swift
+++ Source/GoogleBiddingAdapterBannerAd.swift
@@ -7,15 +7,17 @@ import ChartboostMediationSDK
 import Foundation
 import GoogleMobileAds
 
-class GoogleBiddingAdapterBannerAd: GoogleBiddingAdapterAd, PartnerAd {
-    /// The partner ad view to display inline. E.g. a banner view.
-    /// Should be nil for full-screen ads.
-    var inlineView: UIView?
-    
+class GoogleBiddingAdapterBannerAd: GoogleBiddingAdapterAd, PartnerBannerAd {
+    /// The partner banner ad view to display.
+    var view: UIView?
+
+    /// The loaded partner ad banner size.
+    var size: PartnerBannerSize?
+
     /// Loads an ad.
     /// - parameter viewController: The view controller on which the ad will be presented on. Needed on load for some banners.
     /// - parameter completion: Closure to be performed once the ad has been loaded.
-    func load(with viewController: UIViewController?, completion: @escaping (Result<PartnerEventDetails, Error>) -> Void) {
+    func load(with viewController: UIViewController?, completion: @escaping (Error?) -> Void) {
         log(.loadStarted)
         loadCompletion = completion
 
@@ -23,93 +25,86 @@ class GoogleBiddingAdapterBannerAd: GoogleBiddingAdapterAd, PartnerAd {
         guard viewController != nil else {
             let error = error(.showFailureViewControllerNotFound)
             log(.loadFailed(error))
-            completion(.failure(error))
+            completion(error)
             return
         }
         // Check for valid adm
-        guard request.adm != nil, request.adm != "" else {
+        guard let adm = request.adm, !adm.isEmpty else {
             let error = error(.loadFailureInvalidAdMarkup)
             log(.loadFailed(error))
-            completion(.failure(error))
+            completion(error)
             return
         }
-        
+
         // Create banner
-        let bannerView = GADBannerView(adSize: gadAdSizeFrom(cgSize: request.size, format: request.format))
+        guard
+            let requestedSize = request.bannerSize,
+            let gadSize = requestedSize.gadAdSize
+        else {
+            // Fail if we cannot fit a fixed size banner in the requested size.
+            let error = error(.loadFailureInvalidBannerSize)
+            log(.loadFailed(error))
+            completion(error)
+            return
+        }
+
+        let bannerView = GADBannerView(adSize: gadSize)
         bannerView.adUnitID = request.partnerPlacement
         bannerView.isAutoloadEnabled = false
         bannerView.delegate = self
         bannerView.rootViewController = viewController
-        inlineView = bannerView
-        
+        view = bannerView
+
         // Load banner
         let gbRequest = generateRequest()
         bannerView.load(gbRequest)
     }
-    
-    /// Shows a loaded ad.
-    /// It will never get called for banner ads. You may leave the implementation blank for that ad format.
-    /// - parameter viewController: The view controller on which the ad will be presented on.
-    /// - parameter completion: Closure to be performed once the ad has been shown.
-    func show(with viewController: UIViewController, completion: @escaping (Result<PartnerEventDetails, Error>) -> Void) {
-        // no-op
-    }
-    
-    private func gadAdSizeFrom(cgSize: CGSize?, format: AdFormat) -> GADAdSize {
-        guard let size = cgSize else { return GADAdSizeInvalid }
-
-        if format == .banner {
-            switch size.height {
-            case 50..<90:
-                return GADAdSizeBanner
-            case 90..<250:
-                return GADAdSizeLeaderboard
-            case 250...:
-                return GADAdSizeMediumRectangle
-            default:
-                return GADAdSizeBanner
-            }
-        } else {
-            // Adaptive banner
-            return GADInlineAdaptiveBannerAdSizeWithWidthAndMaxHeight(size.width, size.height)
-        }
-    }
 }
 
 extension GoogleBiddingAdapterBannerAd: GADBannerViewDelegate {
-
     func bannerViewDidReceiveAd(_ bannerView: GADBannerView) {
         log(.loadSucceeded)
         // From https://developers.google.com/admob/ios/api/reference/Functions:
         // "The exact size of the ad returned is passed through the banner’s ad size delegate and
         // is indicated by the banner’s intrinsicContentSize."
-        let loadedSize = bannerView.intrinsicContentSize
-        let partnerDetails = [
-            "bannerWidth": "\(loadedSize.width)",
-            "bannerHeight": "\(loadedSize.height)",
-            // Determine if this is a fluid size ad or not. If the frame of a non-fluid adaptive ad
-            // is changed by even 1px, it will trigger a reload, so we want to avoid this by
-            // returning fixed in this case.
-            // 0 for fixed size banner, 1 for adaptive banner.
-            "bannerType": GADAdSizeIsFluid(bannerView.adSize) ? "1" : "0"
-        ]
-        loadCompletion?(.success(partnerDetails)) ?? log(.loadResultIgnored)
+        size = PartnerBannerSize(
+            size: bannerView.intrinsicContentSize,
+            type: GADAdSizeIsFluid(bannerView.adSize) ? .adaptive : .fixed
+        )
+        loadCompletion?(nil) ?? log(.loadResultIgnored)
         loadCompletion = nil
     }
 
     func bannerView(_ bannerView: GADBannerView, didFailToReceiveAdWithError error: Error) {
-        let error = self.error(.loadFailureException, error: error)
         log(.loadFailed(error))
-        loadCompletion?(.failure(error)) ?? log(.loadResultIgnored)
+        loadCompletion?(error) ?? log(.loadResultIgnored)
         loadCompletion = nil
     }
 
     func bannerViewDidRecordImpression(_ bannerView: GADBannerView) {
         log(.didTrackImpression)
-        delegate?.didTrackImpression(self, details: [:]) ?? log(.delegateUnavailable)
+        delegate?.didTrackImpression(self) ?? log(.delegateUnavailable)
     }
 
     func bannerViewDidRecordClick(_ bannerView: GADBannerView) {
-        delegate?.didClick(self, details: [:]) ?? log(.delegateUnavailable)
+        delegate?.didClick(self) ?? log(.delegateUnavailable)
+    }
+}
+
+extension BannerSize {
+    fileprivate var gadAdSize: GADAdSize? {
+        if self.type == .adaptive {
+            return GADInlineAdaptiveBannerAdSizeWithWidthAndMaxHeight(self.size.width, self.size.height)
+        }
+        switch self {
+        case .standard:
+            return GADAdSizeBanner
+        case .medium:
+            return GADAdSizeMediumRectangle
+        case .leaderboard:
+            return GADAdSizeLeaderboard
+        default:
+            return nil
+        }
     }
 }
